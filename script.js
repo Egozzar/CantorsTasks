66fb4d3b137f34ab533e30c502ff47ac8705b80f
Egozzar <Egozzar@yandex.ru>
2020-10-22 18:09:35 +0300

    Массивы. Подмассив наибольшей суммы.

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 "use strict";
 
-function sumInput() {
-	let arr = [];
+function getMaxSubSum(arr) {
 	let sum = 0;
-	let val;
-
-	while (true) {
-		 val = prompt('Введите значение', '');
-		if (val === null || val === '' || !isFinite(val)) break;
-		arr.push(val);
-	}
+	let maxSum = 0;
 
 	for (let val of arr) {
-		sum += +val;
+		sum += val;
+
+		if (sum < 0) sum = 0;
+		if (sum > maxSum) maxSum = sum;
 	}
 
-	return sum;
+	return maxSum;
 }
 
-alert( sumInput() );
+alert( getMaxSubSum([-1, 2, 3, -9]) );// 5 (сумма выделенных)
+alert( getMaxSubSum([2, -1, 2, 3, -9]) );// 6
+alert( getMaxSubSum([-1, 2, 3, -9, 11]) );// 11
+alert( getMaxSubSum([-2, -1, 1, 2]) );// 3
+alert( getMaxSubSum([100, -9, 2, -3, 5]) );// 100
+alert( getMaxSubSum([1, 2, 3]) );// 6 (берём все)
+alert( getMaxSubSum([-1, -2, -3]) );// 0
