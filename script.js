a7e18d14818beb7561ca30077e8fe3e4e0f5b853
Egozzar <Egozzar@yandex.ru>
2021-04-12 21:15:24 +0300

    Методы массивов.Трансформировать в объекты.

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
 "use strict";
 
-function Calculator() {
-	this.data = {
-		'+' : (a, b) => a + b,
-		'-' : (a, b) => a - b,
+let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+let petya = { name: "Петя", surname: "Иванов", id: 2 };
+let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+let users = [ vasya, petya, masha ];
+
+let usersMapped = users.map( (val) => {
+	return {
+		fullName : `${val.name} ${val.surname}`,
+		id : val.id,
 	}
+})
 
-	this.calculate = function(str) {
-		let arr = str.split(' ');
-		let num1 = +arr[0], num2 = +arr[2], op = arr[1];
+	/*
+	usersMapped = [
+		{ fullName: "Вася Пупкин", id: 1 },
+		{ fullName: "Петя Иванов", id: 2 },
+		{ fullName: "Маша Петрова", id: 3 }
+	]
+	*/
 
-		if (!isFinite(num1) || !isFinite(num2) || !this.data[op]) {
-			return 'Error';
-		}
-
-		return this.data[op](num1, num2);
-	}
-
-	this.addMethod = function(str, func) {
-		this.data[str] = func;
-	}
-}
-
-
-let powerCalc = new Calculator;
-powerCalc.addMethod("*", (a, b) => a * b);
-powerCalc.addMethod("/", (a, b) => a / b);
-powerCalc.addMethod("**", (a, b) => a ** b);
-
-let result = powerCalc.calculate("2 ** 3");
-alert( result ); // 8
+alert( usersMapped[0].id ) // 1
+alert( usersMapped[0].fullName ) // Вася Пупкин
