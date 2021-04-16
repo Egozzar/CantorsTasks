3c05527e18ce1fdf6d53d9df351afddfb8092c8f
Egozzar <Egozzar@yandex.ru>
2021-04-16 21:56:40 +0300

    Дата и время. Сколько сегодня прошло секунд?.

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 "use strict";
 
-function getDateAgo(date, days) {
-	let cloneDate = new Date(date);
-	cloneDate.setDate( cloneDate.getDate() - days);
+function getSecondsToday() {
+	let middlenight = new Date().setHours(0, 0, 0, 0);
 
-	return cloneDate.getDate();
+	return Math.round( (Date.now() - middlenight) / 1000 );
 }
 
-let date = new Date(2015, 0, 2);
 
-alert( getDateAgo(date, 1) ); // 1, (1 Jan 2015)
-alert( getDateAgo(date, 2) ); // 31, (31 Dec 2014)
-alert( getDateAgo(date, 365) ); // 2, (2 Jan 2014)
+alert(getSecondsToday() );
+
 
