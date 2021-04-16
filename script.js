94f0295e0a5799c96e0f1b0172a196b11d6046e9
Egozzar <Egozzar@yandex.ru>
2021-04-16 21:41:26 +0300

    Дата и время. Какой день месяца был много дней назад?.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 "use strict";
 
-function getWeekDay(date) {
-	let arr = ['ВС','ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ',];
-	let day = date.getDay();
+function getDateAgo(date, days) {
+	let cloneDate = new Date(date);
+	cloneDate.setDate( cloneDate.getDate() - days);
 
-	return arr[day];
+	return cloneDate.getDate();
 }
 
-let date = new Date(2012, 0, 3);  // 3 января 2012 года
-alert( getWeekDay(date) );        // нужно вывести "ВТ"
+let date = new Date(2015, 0, 2);
+
+alert( getDateAgo(date, 1) ); // 1, (1 Jan 2015)
+alert( getDateAgo(date, 2) ); // 31, (31 Dec 2014)
+alert( getDateAgo(date, 365) ); // 2, (2 Jan 2014)
 
