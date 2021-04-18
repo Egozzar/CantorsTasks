8c244f1d571a3decdebadd743948f2f6f9d1cd9f
Egozzar <Egozzar@yandex.ru>
2021-04-18 16:11:40 +0300

    Рекурсия и стек. Числа Фибоначчи.

--- script.js
+++ script.js
@@ -1,18 +1,41 @@
 "use strict";
-// цикл
-/*function factorial(n) {
-	let res = 1;
+// цикл с двумя переменными
+/*function fib(n) {
+	let a = 1, b = 1;
 
-	while (n) {
-		res *= n--;
+	for (let i = 3; i <= n; i++) {
+
+		if (a > b) b += a;
+		else a += b;
 	}
 
-	return res;
+	return Math.max(a, b);
 }*/
-// рекурсия
-function factorial(n) {
+// цикл с тремя переменными
+/*function fib(n) {
+	let a = 1, b = 1, c;
 
-	return n ? n * factorial(n - 1) : 1;
+	for (let i = 3; i <= n; i++) {
+		c = a + b;
+		a = b;
+		b = c;
+	}
+
+	return b;
+}*/
+// рекурсия простая
+/*function fib(n) {
+	return n > 2 ? fib(n - 1) + fib(n - 2) : 1;
+}*/
+// рекурсия хвостовая
+function fib(n) {
+	return function fibIn(n, a, b) {
+
+		return n > 0 ? fibIn(n - 1, b, a + b) :
+			     n < 0 ? fibIn(n + 1, b, a - b) : a;
+	}(n, 0, 1);
 }
 
-alert( factorial(5) ); // 120
+
+alert( fib(7) ); // 13
+alert( fib(15) ); // 610
