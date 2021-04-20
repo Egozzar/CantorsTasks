5a4951f5115390054f8df265461bd19f7fc9692c
Egozzar <Egozzar@yandex.ru>
2021-04-20 17:03:51 +0300

    Декораторы и переадресация вызова, call/apply. Тормозящий (throttling) декоратор.

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 "use strict";
 
-function debounce(func, ms) {
-	let drag = null;
-
-	return function(...arg) {
-
-		if (!drag) {
-			func.apply(this, arg);
-			drag = true;
-			setTimeout( () => drag = null, ms);
-		}
-	}
+function f(a) {
+	console.log(a)
 }
 
+function throttle(func, ms) {
+	let cache = [];
+	let drag = null;
 
-let f = debounce(alert, 1000);
+	function rec(...arg) {
+		if (drag) {
+			cache = [this, arg];
+			return;
+		}
+		func.apply(this, arg);
+		drag = true;
 
-f(1); // выполняется немедленно
-f(2); // проигнорирован
+		setTimeout( () => {
+			drag = null;
+			if (cache.length) {
+				rec.apply(cache[0], cache[1]);
+				cache = [];
+			}
+		}, ms);
+	}
 
-setTimeout( () => f(3), 100); // проигнорирован (прошло только 100 мс)
-setTimeout( () => f(4), 1100); // выполняется
-setTimeout( () => f(5), 1500); // проигнорирован (прошло только 400 мс от последнего вызова)
+	return rec;
+}
+// f1000 передаёт вызовы f максимум раз в 1000 мс
+let f1000 = throttle(f, 1000);
+
+f1000(1); // показывает 1
+f1000(2); // (ограничение, 1000 мс ещё нет)
+f1000(3); // (ограничение, 1000 мс ещё нет)
+
+// когда 1000 мс истекли ...
+// ...выводим 3, промежуточное значение 2 было проигнорировано
