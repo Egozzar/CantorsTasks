ff3573fa90ca3ab54407db6aaa6578cbca3950e0
Egozzar <Egozzar@yandex.ru>
2021-04-20 23:09:08 +0300

    Привязка контекста к функции. Исправьте функцию, теряющую "this".

--- script.js
+++ script.js
@@ -1,38 +1,22 @@
 "use strict";
 
-function f(a) {
-	console.log(a)
+function askPassword(ok, fail) {
+	let password = prompt("Password?", '');
+	if (password == "rockstar") ok();
+	else fail();
 }
 
-function throttle(func, ms) {
-	let cache = [];
-	let drag = null;
+let user = {
+	name: 'Вася',
 
-	function rec(...arg) {
-		if (drag) {
-			cache = [this, arg];
-			return;
-		}
-		func.apply(this, arg);
-		drag = true;
+	loginOk() {
+		alert(`${this.name} logged in`);
+	},
 
-		setTimeout( () => {
-			drag = null;
-			if (cache.length) {
-				rec.apply(cache[0], cache[1]);
-				cache = [];
-			}
-		}, ms);
-	}
+	loginFail() {
+		alert(`${this.name} failed to log in`);
+	},
 
-	return rec;
-}
-// f1000 передаёт вызовы f максимум раз в 1000 мс
-let f1000 = throttle(f, 1000);
+};
 
-f1000(1); // показывает 1
-f1000(2); // (ограничение, 1000 мс ещё нет)
-f1000(3); // (ограничение, 1000 мс ещё нет)
-
-// когда 1000 мс истекли ...
-// ...выводим 3, промежуточное значение 2 было проигнорировано
+askPassword(() => user.loginOk(), user.loginFail.bind(user));
