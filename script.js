9fa2a5c138b39d08662cb40bba81d6e301e9d512
Egozzar <Egozzar@yandex.ru>
2021-05-05 09:13:10 +0300

    Встроенные прототипы. Добавьте функциям декорирующий метод "defer()".

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 "use strict";
 
 Function.prototype.defer = function(ms) {
-	setTimeout(this, ms);
+	return function () {
+		setTimeout(this, ms, ...arguments)
+	}.bind(this);
 }
 
-function f() {
-	alert("Hello!");
+function f(a, b) {
+	alert( a + b );
 }
 
-f.defer(1000); // выведет "Hello!" через 1 секунду
+f.defer(1000)(1, 2); // выведет 3 через 1 секунду.
