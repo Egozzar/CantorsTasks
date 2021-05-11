44c9d7bf835b447cf3d11c354021633b9f34a4be
Egozzar <Egozzar@yandex.ru>
2021-05-11 08:59:40 +0300

    Async/await. Вызовите async–функцию из "обычной".

--- script.js
+++ script.js
@@ -1,41 +1,13 @@
 "use strict";
 
-class HttpError extends Error {
-	constructor(response) {
-		super(`${response.status} for ${response.url}`);
-		this.name = 'HttpError';
-		this.response = response;
-	}
+async function wait() {
+	await new Promise(resolve => setTimeout(resolve, 1000));
+
+	return 10;
 }
 
-async function loadJson(url) {
-	let response = await fetch(url);
-
-	if (response.status == 200) {
-		let res = response.join();
-		return res;
-	}
-
-	throw new HttpError(response);
+function f() {
+	wait().then(alert);
 }
 
-// Запрашивать логин, пока github не вернёт существующего пользователя.
-async function demoGithubUser() {
-	while (true) {
-		try {
-			let name = prompt("Введите логин?", "iliakan");
-			let user = await loadJson(`https://api.github.com/users/${name}`);
-			alert(`Полное имя: ${user.name}.`);
-			return user;
-
-		} catch(err) {
-			if (err instanceof HttpError && err.response.status == 404) {
-				alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
-		} else {
-				throw err;
-			}
-		}
-	}
-}
-
-demoGithubUser();
+f();
