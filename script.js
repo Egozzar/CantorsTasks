a226a8ead2f24b80c0c7166c5872e5cba1f4cabb
Egozzar <Egozzar@yandex.ru>
2021-05-11 08:45:47 +0300

    Async/await. Перепишите, используя async/await(2).

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 "use strict";
 
+class HttpError extends Error {
+	constructor(response) {
+		super(`${response.status} for ${response.url}`);
+		this.name = 'HttpError';
+		this.response = response;
+	}
+}
+
 async function loadJson(url) {
 	let response = await fetch(url);
 
-	if (response.status === 200) {
-		let res = await response.json();
-
+	if (response.status == 200) {
+		let res = response.join();
 		return res;
 	}
-	throw new Error(response.status);
+
+	throw new HttpError(response);
 }
 
-loadJson('no-such-user.json') // (3)
-	.catch(alert); // Error: 404
+// Запрашивать логин, пока github не вернёт существующего пользователя.
+async function demoGithubUser() {
+	while (true) {
+		try {
+			let name = prompt("Введите логин?", "iliakan");
+			let user = await loadJson(`https://api.github.com/users/${name}`);
+			alert(`Полное имя: ${user.name}.`);
+			return user;
+
+		} catch(err) {
+			if (err instanceof HttpError && err.response.status == 404) {
+				alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
+		} else {
+				throw err;
+			}
+		}
+	}
+}
+
+demoGithubUser();
