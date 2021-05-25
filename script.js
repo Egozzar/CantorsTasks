89d27a8d53026f8552c92947966760db32b7b202
Egozzar <Egozzar@yandex.ru>
2021-05-25 15:16:14 +0300

    Навигация по DOM-элементам. Выделите ячейки по диагонали.

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 "use strict";
-
-let animals = ["тигр", "ёж", "енот", "ехидна", "АИСТ", "ЯК"];
-
-let collator = new Intl.Collator();
-animals.sort(collator.compare);
-
-alert( animals ); // АИСТ,ёж,енот,ехидна,тигр,ЯК
