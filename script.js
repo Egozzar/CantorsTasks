e9242b8834d5a11415023ecdfbbcb8d8cd553599
Egozzar <Egozzar@yandex.ru>
2021-05-26 14:03:03 +0300

    Поиск: getElement*, querySelector*. Поиск элементов.

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 "use strict";
+
+let table = document.getElementById('age-table');
+alert(table.rows.length);
+
+let labels = table.querySelectorAll('label');
+alert(labels.length);
+
+let age = table.querySelector('td');
+alert(age.innerHTML);
+
+let form = document.body.querySelector('form[name="search"]');
+alert(form.name);
+
+let input = form.querySelector('input');
+alert(input.type);
+
+let lastInput = form.lastElementChild;
+alert(lastInput.type);
+
+
+
+
+
+
+
+
+
+
