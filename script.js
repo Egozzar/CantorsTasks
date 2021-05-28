37e3e821ec54c1314cdb1d4838b5eb914273fd86
Egozzar <Egozzar@yandex.ru>
2021-05-28 19:55:36 +0300

    Изменение документа. Создайте дерево из объекта.

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 "use strict";
 
-function createList() {
-	let ul = document.createElement('ul');
+let data = {
+	"Рыбы": {
+		"форель": {},
+		"лосось": {}
+	},
 
-	while (true) {
-		let li = document.createElement('li');
-		let exp = prompt('Добавь что-нибудь', '');
-		if (!exp) break;
-
-		li.append(exp);
-		ul.append(li);
+	"Деревья": {
+		"Огромные": {
+			"секвойя": {},
+			"дуб": {}
+		},
+		"Цветковые": {
+			"яблоня": {},
+			"магнолия": {}
+		}
 	}
+};
 
-	document.body.append(ul);
+let container = document.getElementById('container');
+
+function createTree(elem, obj) {
+	if (Object.keys(obj).length) {
+		let ul = document.createElement('ul');
+
+		for (let key in obj) {
+			let li = document.createElement('li');
+			li.textContent = key;
+			ul.append(li);
+			createTree(li, obj[key]);
+		}
+		elem.append(ul);
+	}
 }
 
-createList();
+createTree(container, data);
