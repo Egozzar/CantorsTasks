05bc7343e3f32d9a2e3dbea2a04dcf79f626a3f1
Egozzar <Egozzar@yandex.ru>
2021-05-29 15:44:11 +0300

    Изменение документа. Вставьте HTML в список.

--- script.js
+++ script.js
@@ -1,25 +1,4 @@
 "use strict";
 
-let timer;
-
-function run () {
-	let hour = document.querySelector('.hour');
-	let min = document.querySelector('.min');
-	let sec = document.querySelector('.sec');
-
-	let date = new Date();
-	let str = date.toLocaleTimeString();
-
-	hour.innerHTML = str.slice(0, 2);
-	min.innerHTML = str.slice(3, 5);
-	sec.innerHTML = str.slice(6);
-}
-
-function clockStart() {
-	timer = setInterval(run, 1000);
-	run();
-}
-
-function clockStop() {
-	clearInterval(timer);
-}
+let one = document.getElementById('one');
+one.insertAdjacentHTML('afterend', '<li>2</li><li>3</li>');
