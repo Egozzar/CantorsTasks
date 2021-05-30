4c8e372b26cbfdd8b41a300d3ed9ff2f7d05fa6b
Egozzar <Egozzar@yandex.ru>
2021-05-31 00:01:38 +0300

    Изменение документа. Сортировка таблицы.

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 "use strict";
+function sorting(element) {
+	let trs = Array.from(element.rows);
+	let trsSort = trs.slice(1).sort( (a, b) => a.cells[0].innerHTML > b.cells[0].innerHTML ? 1 : -1);
+	trsSort.unshift(trs[0]);
 
-let one = document.getElementById('one');
-one.insertAdjacentHTML('afterend', '<li>2</li><li>3</li>');
+	trsSort.forEach((elem) => {
+		element.firstElementChild.append(elem);
+	});
+}
+
+let table = document.getElementById('table');
+setTimeout(sorting, 2000, table);
+
+
