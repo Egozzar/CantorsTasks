2543ad9f2e75a849fae08098eb7460e98040905b
Egozzar <Egozzar@yandex.ru>
2021-06-05 00:04:19 +0300

    Введение в браузерные события. Передвиньте мяч по полю.

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 "use strict";
 
-let button = document.querySelector('[type="button"]');
+let field = document.getElementById('field');
+let ball = document.getElementById('ball');
 
-button.onclick = function() {this.hidden = true};
+field.style.cursor = 'pointer';
+ball.style.position = 'absolute';
+field.style.position = 'relative';
+ball.style.left = 0;
+ball.style.top = 0;
+
+field.addEventListener('click', touchBall);
+
+function touchBall(event) {
+	let paramBall = ball.getBoundingClientRect();
+	let paramField = this.getBoundingClientRect();
+
+	let posX = event.clientX - paramField.left - field.clientLeft - (paramBall.width / 2 );
+	let posY = event.clientY - paramField.top - field.clientTop - (paramBall.height / 2 );
+
+	if (posX < 0) posX = 0;
+
+	if (posX  > field.clientWidth - paramBall.width) {
+		posX = field.clientWidth - paramBall.width;
+	}
+
+	if (posY < 0) posY = 0;
+
+	if (posY > field.clientHeight - paramBall.height) {
+		posY = field.clientHeight - paramBall.height;
+	}
+
+	ball.style.left = posX + 'px';
+	ball.style.top = posY + 'px';
+}
