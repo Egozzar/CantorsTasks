abe0e9ac28bd591599e4de147002a3e7da647bd0
Egozzar <Egozzar@yandex.ru>
2021-06-04 12:47:06 +0300

    Введение в браузерные события. Спрятать себя.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 "use strict";
 
 let button = document.querySelector('[type="button"]');
-let div = document.getElementById('text');
 
-button.addEventListener('click', () => div.style.display = "none");
+button.onclick = function() {this.hidden = true};
