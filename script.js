6402532307688fd6c13fb01e0babf300d29e1ec9
Egozzar <Egozzar@yandex.ru>
2021-06-08 00:24:31 +0300

    Действия браузера по умолчанию. Поймайте переход по ссылке.

--- script.js
+++ script.js
@@ -1,35 +1,11 @@
 "use strict";
 
-let tooltip;
+let contents = document.getElementById('contents');
 
-document.addEventListener('mouseover', (event) => {
-	let tar = event.target.closest('[data-tooltip]');
+contents.addEventListener('click', (event) => {
+	let tar = event.target.closest('a');
 	if (!tar) return;
 
-	tooltip = document.createElement('div');
-	tooltip.className = 'tooltip';
-	tooltip.innerHTML = tar.dataset.tooltip;
-	document.body.append(tooltip);
-
-	positioning(tooltip, tar);
-});
-
-document.addEventListener('mouseout', () => {
-	if (tooltip) {
-		tooltip.remove();
-		tooltip = null;
-	}
-});
-
-function positioning(elem, anchor) {
-	let coordAnchor = anchor.getBoundingClientRect();
-
-	let left = coordAnchor.left + coordAnchor.width / 2 - elem.offsetWidth / 2;
-	if (left < 0) left = 0;
-
-	let top = coordAnchor.top - elem.offsetHeight - 5;
-	if (top < 0) top = coordAnchor.bottom + 5;
-
-	elem.style.left = left + 'px';
-	elem.style.top = top + 'px';
-}
+	let href = tar.href;
+	if ( !confirm(`Перейти на страницу ${href} ?`) ) event.preventDefault();
+})
