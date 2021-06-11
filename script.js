a61e83e14b54a3b19d1c5fd63da7be8560390014
Egozzar <Egozzar@yandex.ru>
2021-06-11 12:53:03 +0300

    Движение мыши: mouseover/out, mouseenter/leave. Улучшенная подсказка.

--- script.js
+++ script.js
@@ -1,22 +1,45 @@
 "use strict";
 
-let ul = document.getElementById('ul');
-let lis = ul.querySelectorAll('li');
-let pressed;
+let house = document.getElementById('house');
+house.style.position = 'relative';
+let overhang = null;
 
-ul.addEventListener('mousedown', (event) => {
-	event.preventDefault();
-});
+house.addEventListener('mouseout', (event) => {
+	if (!overhang) return;
 
-ul.addEventListener('click', (event) => {
-	let tar = event.target.closest('li');
-	if (!tar) return;
+	let newPlace = event.relatedTarget;
 
-	let ctrl = event.ctrlKey || event.metaKey;
-	if (pressed && !ctrl) {
-		lis.forEach(li => li.classList.remove('selected'));
+	while (newPlace) {
+		if (newPlace == overhang) return;
+		newPlace = newPlace.parentNode;
 	}
 
-	pressed = tar;
-	pressed.classList.toggle('selected');
+	overhang.remove();
+	overhang = null;
 });
+
+house.addEventListener('mouseover', (event) => {
+	let tar = event.target.closest('[data-tooltip]');
+	if (!tar) return;
+
+	overhang = document.createElement('div');
+	overhang.classList.add('tooltip');
+	overhang.innerHTML = tar.dataset.tooltip;
+	document.body.append(overhang);
+
+	positioning(overhang, tar);
+});
+
+function positioning(elem, anchor) {
+	let coordAnchor = anchor.getBoundingClientRect();
+	let left = coordAnchor.left + coordAnchor.width / 2 - elem.offsetWidth / 2;
+	if (left < 0) left = 0;
+
+	let top = coordAnchor.top - elem.offsetHeight - 10;
+	if (top < 0) {
+		top = coordAnchor.bottom + 10;
+	}
+
+	elem.style.left = left + 'px';
+	elem.style.top = top + 'px';
+}
