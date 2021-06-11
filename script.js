a0df073639fbadc278b6ae83fc3bb519ee3bf362
Egozzar <Egozzar@yandex.ru>
2021-06-12 00:53:36 +0300

    Клавиатура: keydown и keyup. Отследить одновременное нажатие.

--- script.js
+++ script.js
@@ -1,45 +1,26 @@
 "use strict";
 
-let house = document.getElementById('house');
-house.style.position = 'relative';
-let overhang = null;
+function runOnKeys(func, ...args) {
+	let sum = 0;
 
-house.addEventListener('mouseout', (event) => {
-	if (!overhang) return;
+	document.addEventListener('keydown', (event) => {
+		if ( args.includes(event.code) && !event.repeat ) sum++;
 
-	let newPlace = event.relatedTarget;
+		if (sum == args.length) {
+			sum = 0;
+			func();
+		}
+	});
 
-	while (newPlace) {
-		if (newPlace == overhang) return;
-		newPlace = newPlace.parentNode;
-	}
+	document.addEventListener('keyup', (event) => {
+		if ( args.includes(event.code) ) sum--;
 
-	overhang.remove();
-	overhang = null;
-});
-
-house.addEventListener('mouseover', (event) => {
-	let tar = event.target.closest('[data-tooltip]');
-	if (!tar) return;
-
-	overhang = document.createElement('div');
-	overhang.classList.add('tooltip');
-	overhang.innerHTML = tar.dataset.tooltip;
-	document.body.append(overhang);
-
-	positioning(overhang, tar);
-});
-
-function positioning(elem, anchor) {
-	let coordAnchor = anchor.getBoundingClientRect();
-	let left = coordAnchor.left + coordAnchor.width / 2 - elem.offsetWidth / 2;
-	if (left < 0) left = 0;
-
-	let top = coordAnchor.top - elem.offsetHeight - 10;
-	if (top < 0) {
-		top = coordAnchor.bottom + 10;
-	}
-
-	elem.style.left = left + 'px';
-	elem.style.top = top + 'px';
+		sum = sum < 0 ? 0 : sum;
+	});
 }
+
+runOnKeys(
+	() => alert("Привет!"),
+	"KeyQ",
+	"KeyW"
+);
